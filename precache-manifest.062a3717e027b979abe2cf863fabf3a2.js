08cb9069c377fa493d051fbba194d65b3de5d2f2
Makas9 <46976305+Makas9@users.noreply.github.com>
2020-09-21 16:41:00 +0300

    Updates

---
+++ precache-manifest.062a3717e027b979abe2cf863fabf3a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f288675020f0d419d259205f6d085c7",
+    "url": "/FlightWatch/index.html"
+  },
+  {
+    "revision": "116e8c771a7f246892fd",
+    "url": "/FlightWatch/static/css/2.79966c17.chunk.css"
+  },
+  {
+    "revision": "1cfa5ffd896f8ec1fd8d",
+    "url": "/FlightWatch/static/css/main.f37afe95.chunk.css"
+  },
+  {
+    "revision": "116e8c771a7f246892fd",
+    "url": "/FlightWatch/static/js/2.0dbce0f4.chunk.js"
+  },
+  {
+    "revision": "226448158be1890ba2ad5ace5b26b46f",
+    "url": "/FlightWatch/static/js/2.0dbce0f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cfa5ffd896f8ec1fd8d",
+    "url": "/FlightWatch/static/js/main.2b941dc5.chunk.js"
+  },
+  {
+    "revision": "7c6d33a4246b98139885",
+    "url": "/FlightWatch/static/js/runtime-main.dadcace5.js"
+  }
+]);
